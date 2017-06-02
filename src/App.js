65f218c25890f850a65dc14d2b812017562de1b4
Jennifer Pemberton <jpemberton76@gmail.com>
2017-06-02 16:35:18 -0400

    wired up ReduxThunk with provider error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
@@ -18,14 +18,15 @@ class App extends Component {
     };
     firebase.initializeApp(config);
   }
-  
+
   render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <LoginForm/>
-      </Provider>
-    );
-  }
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+      return (
+        <Provider store={store}>
+          <LoginForm/>
+        </Provider>
+      );
+    }
 }
 
 export default App;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text){
+    this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render(){
@@ -25,12 +35,14 @@ class LoginForm extends Component {
           <Input
             secureTextEntry
             label="Password"
-            placeholder="Password"
-            />
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -42,7 +54,11 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   }
 };
-export default connect(mapStateToProps, { emailChanged }) (LoginForm);
+
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser
+}) (LoginForm);
