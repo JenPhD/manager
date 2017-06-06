5d17afd8f13db4532aede171572a041c85fbe4d2
Jennifer Pemberton <jpemberton76@gmail.com>
2017-06-06 00:34:20 -0400

    list of employees rendering

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,25 +1,61 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
+
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //nextProps are the next set of props that this component
+    //will be rendered with
+    //this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    //ds = datasource
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    //rows expect an array, not object returned from firebase
+    //need to convert using lodash in mapStatetoProps below
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <ListItem employee={employee} />
   }
 
   render() {
+    console.log(this.props);
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+        />
     );
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  //using lodash to convert emplyees from object to array
+  //val is the employee model, uid is the key or unique id
+  const employees = _.map(state.employees, (val, uid) => {
+    //push in all the values and return id, map puts into array
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
